6741bc523c6f718de0da85750eb7bcca2aff7d15
sing-fung <1587898817@qq.com>
2021-12-20 12:18:39 -0500

    Initial commit

---
+++ src/test/java/com/livebarn/blogposts/BlogPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livebarn.blogposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
